77d2b8e20016d4bce80e966e0814448bc4709a76
itmo153277 <153277@niuitmo.ru>
2016-02-21 14:56:09 +0300

    *Documentation for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+/**
+ * @file main.cpp
+ * @brief Основная точка входа в программу
+ *
+ * Точка входа в программу
+ */
+
 /*
  A simple bot playing Go
- Copyright (C) 2015 Ivanov Viktor
+ Copyright (C) 2016 Ivanov Viktor
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -16,6 +23,13 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+/**
+ * Точка входа в программу
+ *
+ * @param argc Количество аргументов
+ * @param argv Аргументы
+ * @return Код ошибки
+ */
 int main(int argc, char *argv[]) {
 	return 0;
 }
