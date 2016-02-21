e2dc26ee51aefdf0b3964bfc59883ed6df1026c8
itmo153277 <153277@niuitmo.ru>
2016-02-21 14:21:30 +0300

    +Added test parser

--- tests/tester.cpp
+++ tests/tester.cpp
@@ -7,7 +7,7 @@
 
 /*
  A simple bot playing Go
- Copyright (C) 2015 Ivanov Viktor
+ Copyright (C) 2016 Ivanov Viktor
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -23,18 +23,60 @@
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <boost/test/included/unit_test.hpp>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cerrno>
+#include "tester.h"
 
-using namespace boost::unit_test;
+using namespace ::boost::unit_test;
+using namespace ::std;
+using namespace ::vpgo::tests;
 
 /**
  * Функция парсинга тестового файла и регистрации тестов
+ *
  * @param argc Количество аргументов тестера
  * @param argv Аргументы тестера
  * @return Всегда NULL, так как все тесты регистрируются самостоятельно
  */
 test_suite*
-init_unit_test_suite( int argc, char* argv[] )
-{
-    return 0;
+init_unit_test_suite(int argc, char* argv[]) {
+	if (argc < 2) {
+		return NULL;
+	}
+
+	ifstream inp(argv[1]);
+	stringstream json;
+
+	if (inp.fail()) {
+		return NULL;
+	}
+	json << inp.rdbuf();
+
+	Json::Reader Reader;
+	Json::Value Root;
+
+	if (!Reader.parse(json.str(), Root)) {
+		return NULL;
+	}
+
+	static const int ParsersCount = 0;
+	static const char *ParserAliases[ParsersCount] = { };
+	ParserHandler ParserHandlers[ParsersCount] = { };
+
+	for (auto TestSuite : Root["suites"]) {
+		string SuiteName = TestSuite["name"].asString();
+		for (int i = 0; i < ParsersCount; i++) {
+			if (SuiteName == ParserAliases[i]) {
+				framework::master_test_suite().add(
+						(*ParserHandlers[i])(TestSuite));
+				break;
+			}
+		}
+	}
+	return NULL;
 }
