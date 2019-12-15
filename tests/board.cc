b22953e27742819b5779adc332c0a72dfc1e27cd
Ivanov Viktor <viktprog@gmail.com>
2019-12-15 18:05:50 +0900

    Add basic board tests

--- tests/board.cc
+++ tests/board.cc
@@ -23,6 +23,8 @@
 #include <cstddef>
 #include <utility>
 #include <iostream>
+#include <string>
+#include <algorithm>
 #include <boost/test/unit_test.hpp>
 #include <boost/test/data/test_case.hpp>
 #include "../src/board.hpp"
@@ -53,13 +55,101 @@ ostream &operator<<(ostream &os, BoardTraverse::iterator::value_type v) {
  */
 ostream &operator<<(
     ostream &os, const vector<BoardTraverse::iterator::value_type> &v) {
-	for (auto x : v) {
-		os << x << "; ";
-	}
+	std::copy(v.begin(), v.end(),
+	    std::ostream_iterator<BoardTraverse::iterator::value_type>(os, ", "));
 	return os;
 }
 
 }  // namespace std
+
+/**
+ * Board iterator
+ */
+struct BoardIterator {
+	using iterator_category = std::forward_iterator_tag;
+	using value_type = PlayerColour;
+	using reference = void;
+	using pointer = void;
+	using difference_type = int;
+
+	std::size_t i;
+	const Board *board;
+
+	bool operator!=(const BoardIterator &other) {
+		return i != other.i;
+	}
+	BoardIterator &operator++() {
+		++i;
+		return *this;
+	}
+	BoardIterator operator++(int) {
+		auto copy = *this;
+		++i;
+		return copy;
+	}
+	value_type operator*() {
+		return board->getValue(i);
+	}
+};
+
+/**
+ * Convert board to vector
+ *
+ * @param board Board
+ * @return Vector of values
+ */
+std::vector<PlayerColour> convertToVector(const Board &board) {
+	std::vector<PlayerColour> data(BoardIterator({0, &board}),
+	    BoardIterator({board.getSize() * board.getSize(), &board}));
+
+	return data;
+}
+
+/**
+ * Convert board to string
+ * 
+ * @param board Board
+ * @return String 
+ */
+std::string convertToString(const Board &board) {
+	std::vector<char> data;
+	std::transform(BoardIterator({0, &board}),
+	    BoardIterator({board.getSize() * board.getSize(), &board}),
+	    std::back_inserter(data), [](PlayerColour c) {
+		    switch (c) {
+		    case PlayerColour::BLACK:
+			    return 'B';
+		    case PlayerColour::WHITE:
+			    return 'W';
+		    default:
+			    return ' ';
+		    }
+	    });
+	return std::string(data.data(), data.size());
+}
+
+/**
+ * Convert string to board
+ * 
+ * @param size Board size
+ * @param s String
+ * @return Board 
+ */
+Board convertFromString(std::size_t size, const std::string &s) {
+	Board b(size);
+	for (std::size_t i = 0, y = 0; y < size; ++y) {
+		for (std::size_t x = 0; x < size; ++i, ++x) {
+			switch (s[i]) {
+			case 'B':
+				b.playMove(x, y, PlayerColour::BLACK);
+				break;
+			case 'W':
+				b.playMove(x, y, PlayerColour::WHITE);
+			}
+		}
+	}
+	return b;
+}
 
 BOOST_AUTO_TEST_SUITE(board_tests)
 
@@ -122,5 +212,55 @@ BOOST_DATA_TEST_CASE(
 
 // Compile-time check
 static_assert(std::get<0>(*(BoardTraverse(1, 1, 6, 5).begin())) == 0);
+
+/**
+ * Test board position
+ */
+const char testBoard[] =
+    "   "
+    " B "
+    "WW ";
+
+BOOST_AUTO_TEST_CASE(board_position) {
+	Board b = convertFromString(3, testBoard);
+	auto b_string = convertToString(b);
+	BOOST_TEST(b_string == testBoard);
+}
+
+BOOST_AUTO_TEST_CASE(board_copy_ctor) {
+	Board b = convertFromString(3, testBoard);
+	Board copy = b;
+	auto b_data = convertToVector(b);
+	auto copy_data = convertToVector(copy);
+	BOOST_TEST(b_data == copy_data, boost::test_tools::per_element());
+}
+
+BOOST_AUTO_TEST_CASE(board_assign_ctor) {
+	Board b = convertFromString(3, testBoard);
+	Board copy(3);
+	copy = b;
+	auto b_data = convertToVector(b);
+	auto copy_data = convertToVector(copy);
+	BOOST_TEST(b_data == copy_data, boost::test_tools::per_element());
+}
+
+BOOST_AUTO_TEST_CASE(board_move_ctor) {
+	Board b = convertFromString(3, testBoard);
+	Board temp = b;
+    Board copy = std::move(temp);
+	auto b_data = convertToVector(b);
+	auto copy_data = convertToVector(copy);
+	BOOST_TEST(b_data == copy_data, boost::test_tools::per_element());
+}
+
+BOOST_AUTO_TEST_CASE(board_move_assign_ctor) {
+	Board b = convertFromString(3, testBoard);
+	Board temp = b;
+    Board copy(3);
+    copy = std::move(temp);
+	auto b_data = convertToVector(b);
+	auto copy_data = convertToVector(copy);
+	BOOST_TEST(b_data == copy_data, boost::test_tools::per_element());
+}
 
 BOOST_AUTO_TEST_SUITE_END()
