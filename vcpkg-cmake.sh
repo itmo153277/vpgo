2d260734f3417f455eaec8aa936a8085fe371ea1
Viktor Ivanov <viktprog@gmail.com>
2024-09-21 22:07:26 +0900

    Set vcpkg baseline

--- vcpkg-cmake.sh
+++ vcpkg-cmake.sh
@@ -5,9 +5,20 @@ set -euo pipefail
 # CMake wrapper for vcpkg
 
 SCRIPT_DIR=$(dirname "$0")
+VCPKG_BASELINE=98aa6396292d57e737a6ef999d4225ca488859d5
+
+function clone_vcpkg() {
+  mkdir -p "${SCRIPT_DIR}/.vcpkg"
+  pushd "${SCRIPT_DIR}/.vcpkg" >/dev/null
+  git init -q
+  git remote add origin https://github.com/microsoft/vcpkg.git
+  git fetch -q origin "${VCPKG_BASELINE}" --depth 1
+  git checkout -q FETCH_HEAD
+  popd >/dev/null
+}
 
 function setup_vcpkg() {
-  [[ -d "${SCRIPT_DIR}/.vcpkg" ]] || git clone https://github.com/microsoft/vcpkg.git --depth 1 ${SCRIPT_DIR}/.vcpkg
+  [[ -d "${SCRIPT_DIR}/.vcpkg" ]] || clone_vcpkg
   [[ -f "${SCRIPT_DIR}/.vcpkg/vcpkg" ]] || "${SCRIPT_DIR}/.vcpkg/bootstrap-vcpkg.sh" -disableMetrics
   export VCPKG_ROOT=${SCRIPT_DIR}/.vcpkg
 }
