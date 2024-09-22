8455c8220536bec0d2c1be49d8e568c7bdfa3e7e
Viktor Ivanov <viktprog@gmail.com>
2024-09-22 12:18:51 +0900

    Update vcpkg script for windows

--- vcpkg-cmake.bat
+++ vcpkg-cmake.bat
@@ -4,6 +4,8 @@
 
 SetLocal
 
+Set VCPKG_BASELINE=98aa6396292d57e737a6ef999d4225ca488859d5
+
 If "%VCPKG_ROOT%" == "" Call :SetupVCPKG
 If ErrorLevel 1 GoTo :EOF
 For /F "delims=" %%a In ('"%VCPKG_ROOT%\vcpkg" fetch cmake --x-stderr-status') Do Call :ParseOutput "%%~a"
@@ -14,8 +16,22 @@ If ErrorLevel 1 GoTo Fail
 EndLocal
 GoTo :EOF
 
+:CloneVCPKG
+If Not Exist "%~dp0.vcpkg" MkDir "%~dp0.vcpkg"
+PushD "%~dp0.vcpkg"
+Call :CheckedCall git init -q -b main
+If ErrorLevel 1 GoTo :EOF
+Call :CheckedCall git remote add origin https://github.com/microsoft/vcpkg.git
+If ErrorLevel 1 GoTo :EOF
+Call :CheckedCall git fetch -q origin "%VCPKG_BASELINE%" --depth 1
+If ErrorLevel 1 GoTo :EOF
+Call :CheckedCall git checkout -q FETCH_HEAD
+If ErrorLevel 1 GoTo :EOF
+PopD
+GoTo :EOF
+
 :SetupVCPKG
-If Not Exist "%~dp0.vcpkg\*" Call :CheckedCall git clone https://github.com/microsoft/vcpkg.git --depth 1 "%~dp0.vcpkg"
+If Not Exist "%~dp0.vcpkg\*" Call :CLoneVCPKG
 If ErrorLevel 1 GoTo :EOF
 if Not Exist "%~dp0.vcpkg\vcpkg.exe" Call :CheckedCall Call "%~dp0.vcpkg\bootstrap-vcpkg.bat" -disableMetrics
 If ErrorLevel 1 GoTo :EOF
--- vcpkg-cmake.sh
+++ vcpkg-cmake.sh
@@ -10,7 +10,7 @@ VCPKG_BASELINE=98aa6396292d57e737a6ef999d4225ca488859d5
 function clone_vcpkg() {
   mkdir -p "${SCRIPT_DIR}/.vcpkg"
   pushd "${SCRIPT_DIR}/.vcpkg" >/dev/null
-  git init -q
+  git init -q -b main
   git remote add origin https://github.com/microsoft/vcpkg.git
   git fetch -q origin "${VCPKG_BASELINE}" --depth 1
   git checkout -q FETCH_HEAD
